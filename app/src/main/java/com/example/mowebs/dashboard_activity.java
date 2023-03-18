78a3001bd90d73fa52957587cfec97dcb00a8144
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-03-18 13:56:16 +0700

    Dashboard
    Dashboard-Activity

---
+++ app/src/main/java/com/example/mowebs/dashboard_activity.java
@@ -0,0 +1,74 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		dashboard
+	 *	@date 		Saturday 18th of March 2023 02:52:17 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.mowebs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class dashboard_activity extends Activity {
+
+	
+	private View _bg__dashboard_ek2;
+	private TextView _20_01;
+	private ImageView battery;
+	private ImageView phone;
+	private TextView dashboard_ek4;
+	private TextView see_all;
+	private TextView recomended;
+	private ImageView line_5;
+	private ImageView line_6;
+	private ImageView __img___button_car;
+	private ImageView __img___button;
+	private ImageView vector;
+	private TextView customer_service;
+	private ImageView vector_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+
+		
+		_bg__dashboard_ek2 = (View) findViewById(R.id._bg__dashboard_ek2);
+		_20_01 = (TextView) findViewById(R.id._20_01);
+		battery = (ImageView) findViewById(R.id.battery);
+		phone = (ImageView) findViewById(R.id.phone);
+		dashboard_ek4 = (TextView) findViewById(R.id.dashboard_ek4);
+		see_all = (TextView) findViewById(R.id.see_all);
+		recomended = (TextView) findViewById(R.id.recomended);
+		line_5 = (ImageView) findViewById(R.id.line_5);
+		line_6 = (ImageView) findViewById(R.id.line_6);
+		__img___button_car = (ImageView) findViewById(R.id.__img___button_car);
+		__img___button = (ImageView) findViewById(R.id.__img___button);
+		vector = (ImageView) findViewById(R.id.vector);
+		customer_service = (TextView) findViewById(R.id.customer_service);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
