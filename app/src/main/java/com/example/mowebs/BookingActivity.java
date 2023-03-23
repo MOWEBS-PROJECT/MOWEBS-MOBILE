a4e75f95c198d01e786ce4a70a1209d19acaf30d
sukriyatma <muhammadsukriyatma@gmail.com>
2023-03-23 14:13:48 +0700

    Fixing some file

--- app/src/main/java/com/example/mowebs/BookingActivity.java
+++ app/src/main/java/com/example/mowebs/BookingActivity.java
@@ -17,7 +17,8 @@ public class BookingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_booking);
+        setContentView(R.layout.);
+
 
         Button button = findViewById(R.id.btn_payment);
 
