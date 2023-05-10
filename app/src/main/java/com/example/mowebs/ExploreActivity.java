aec7079f3b0886c9686faff9cec0d43c5e70aad8
sukriyatma <muhammadsukriyatma@gmail.com>
2023-05-10 20:48:10 +0700

    new navigation menu

--- app/src/main/java/com/example/mowebs/ExploreActivity.java
+++ app/src/main/java/com/example/mowebs/ExploreActivity.java
@@ -17,36 +17,6 @@ public class ExploreActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_explore);
 
-        navHome = findViewById(R.id.navigation_home);
-        navSearch = findViewById(R.id.navigation_search);
-        navMessage = findViewById(R.id.nagivation_message);
-        navProfile = findViewById(R.id.nagivation_profile);
 
-        navHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ExploreActivity.this, Dashboard.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        navMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ExploreActivity.this, ChatActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        navProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ExploreActivity.this, ProfileActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
--- app/src/main/java/com/example/mowebs/MainActivity.java
+++ app/src/main/java/com/example/mowebs/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.mowebs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigationMenu);
+        frameContent = findViewById(R.id.fragmentContent);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        fragment = new DashboardFragment();
+                        break;
+                    case R.id.navigation_search:
+                        fragment = new ExplorerFragment();
+                        break;
+                    case R.id.navigation_message:
+                        fragment = new ChatFragment(MainActivity.this);
+                        break;
+                }
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContent, fragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
+//        bottomNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                return true;
+//            }
+//        });
+
     }
 }
