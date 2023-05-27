e91691a4d5b875c0933be916f2025a9ee3557cd0
sukriyatma <muhammadsukriyatma@gmail.com>
2023-05-27 12:14:25 +0700

    configurasi dan implementasi API RegisterActivity SignInActivity

--- app/src/main/java/com/example/mowebs/RegisterActivity.java
+++ app/src/main/java/com/example/mowebs/RegisterActivity.java
@@ -3,27 +3,121 @@ package com.example.mowebs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText editTextUsername, editTextEmail, editTextPassword;
+    Button signupButton;
+    TextView tvSignInText;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button btnSignup = findViewById(R.id.btn_signup);
+        editTextEmail    = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        signupButton     = findViewById(R.id.signupButton);
+        tvSignInText     = findViewById(R.id.tvSignInText);
+        requestQueue     = Volley.newRequestQueue(RegisterActivity.this);
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        tvSignInText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
                 startActivity(intent);
-
+                finish();
             }
         });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextUsername.getText().toString().equals("")) {
+                    editTextUsername.setError("Username is empty");
+                    editTextUsername.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                    return;
+                }
+                if (editTextPassword.getText().toString().equals("")) {
+                    editTextPassword.setError("Password is empty");
+                    editTextPassword.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                    return;
+                }
+                if (editTextEmail.getText().toString().equals("")) {
+                    editTextEmail.setError("Email is empty");
+                    editTextEmail.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                    return;
+                }
+
+                signupButton.setEnabled(false);
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        String path = "/register";
+
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("username", editTextUsername.getText().toString());
+            jsonBody.put("password", editTextPassword.getText().toString());
+            jsonBody.put("email", editTextEmail.getText().toString());
+            String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, RequestDatabase.ENDPOINT + path,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(RegisterActivity.this, "Success Register", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse.statusCode == 409) {
+                            editTextUsername.setError("Username already exist");
+                            Toast.makeText(RegisterActivity.this, "Username already exist", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Toast.makeText(RegisterActivity.this, "Failed to register", Toast.LENGTH_SHORT).show();
+                    }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    return requestBody.getBytes(StandardCharsets.UTF_8);
+                }
+            };
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {}
+
     }
 }
--- app/src/main/java/com/example/mowebs/SignInActivity.java
+++ app/src/main/java/com/example/mowebs/SignInActivity.java
@@ -18,38 +18,44 @@
 package com.example.mowebs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SignInActivity extends Activity {
+import androidx.annotation.Nullable;
 
-	
-	private View _bg__sign_in_ek2;
-	private View rectangle_6;
-	private TextView enter_your_username;
-	private ImageView vector;
-	private ImageView vector_ek1;
-	private View rectangle_6_ek1;
-	private TextView enter_your_password;
-	private ImageView vector_ek2;
-	private View rectangle_8;
-	private TextView logo_ek1;
-	private TextView welcome_;
-	private TextView _20_01;
-	private ImageView battery;
-	private ImageView phone;
-	private TextView sign_in_ek3;
-	private TextView don_t_have_an_account__sign_up;
-	private View rectangle_7;
-	private TextView sign_in_ek4;
-	private TextView forgot_password__;
-	private TextView remember_me_ek1;
-	private View rectangle_5;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+	public class SignInActivity extends Activity {
+
+	EditText editTextUsername, editTextPassword;
+	Button signinButton, signUpButton;
+//	TextView tvSignUpText;
+	RequestQueue requestQueue;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,36 +63,97 @@ public class SignInActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_in);
 
-		
-		_bg__sign_in_ek2 = (View) findViewById(R.id._bg__sign_in_ek2);
-		rectangle_6 = (View) findViewById(R.id.rectangle_6);
-		enter_your_username = (TextView) findViewById(R.id.enter_your_username);
-		vector = (ImageView) findViewById(R.id.vector);
-		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
-		rectangle_6_ek1 = (View) findViewById(R.id.rectangle_6_ek1);
-		enter_your_password = (TextView) findViewById(R.id.enter_your_password);
-		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
-		rectangle_8 = (View) findViewById(R.id.rectangle_8);
-		logo_ek1 = (TextView) findViewById(R.id.logo_ek1);
-		welcome_ = (TextView) findViewById(R.id.welcome_);
-		don_t_have_an_account__sign_up = (TextView) findViewById(R.id.don_t_have_an_account__sign_up);
-		sign_in_ek4 = (TextView) findViewById(R.id.sign_in_ek4); //SignIn Button
-		forgot_password__ = (TextView) findViewById(R.id.forgot_password__);
-		remember_me_ek1 = (TextView) findViewById(R.id.remember_me_ek1);
-		rectangle_5 = (View) findViewById(R.id.rectangle_5);
-	
-		
-		//custom code goes here
-		sign_in_ek4.setOnClickListener(new View.OnClickListener()
-		{
+		signUpButton = findViewById(R.id.signUpButton);
+		signinButton = findViewById(R.id.signinButton);
+		editTextUsername = findViewById(R.id.editTextUsername);
+		editTextPassword = findViewById(R.id.editTextPassword);
+		requestQueue	 = Volley.newRequestQueue(SignInActivity.this);
+
+		signUpButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
-//				Pindah ke halaman dashboard
-				Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+			public void onClick(View view) {
+				Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
 				startActivity(intent);
+//				finish();
+
 			}
 		});
+
+		signinButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (editTextUsername.getText().toString().equals("")) {
+					editTextUsername.setError("Username is empty");
+					editTextUsername.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+					return;
+				}
+				if (editTextPassword.getText().toString().equals("")) {
+					editTextPassword.setError("Password is empty");
+					editTextPassword.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+					return;
+				}
+				signinButton.setEnabled(false);
+				signIn();
+			}
+		});
+
+	}
+
+
+	private void signIn() {
+		String path = "/login";
+
+		try {
+			JSONObject jsonBody = new JSONObject();
+			jsonBody.put("username", editTextUsername.getText().toString());
+			jsonBody.put("password", editTextPassword.getText().toString());
+			String requestBody = jsonBody.toString();
+			StringRequest stringRequest = new StringRequest(Request.Method.POST, RequestDatabase.ENDPOINT + path,
+					new Response.Listener<String>() {
+						@Override
+						public void onResponse(String response) {
+							try {
+								JSONObject jsonResponse = new JSONObject(response);
+								String userID = jsonResponse.getString("_id");
+								Preference.setUserId(SignInActivity.this, userID);
+								Preference.setUsername( SignInActivity.this, editTextUsername.getText().toString());
+
+								Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+								startActivity(intent);
+								finish();
+
+							} catch (JSONException e) {
+								Toast.makeText(SignInActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+							}
+
+						}
+					}, new Response.ErrorListener() {
+				@Override
+				public void onErrorResponse(VolleyError error) {
+					if (error.networkResponse.statusCode == 404) {
+						editTextUsername.setError("Username is wrong");
+						editTextUsername.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+						editTextPassword.setError("Password is wrong");
+						editTextPassword.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+						Toast.makeText(SignInActivity.this, "Username and Password is wrong", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					Toast.makeText(SignInActivity.this, "Failed to authentication", Toast.LENGTH_SHORT).show();
+				}
+			}) {
+				@Override
+				public String getBodyContentType() {
+					return "application/json; charset=utf-8";
+				}
+				@Override
+				public byte[] getBody() throws AuthFailureError {
+					return requestBody.getBytes(StandardCharsets.UTF_8);
+				}
+			};
+
+			requestQueue.add(stringRequest);
+		} catch (JSONException e) {}
+
 	}
 }
 	
